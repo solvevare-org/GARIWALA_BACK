ffc4b74de0e3dbbecb75a42c03cbafca4e008fb8
rahatrajput786125 <rahatrajput786125@gmail.com>
2026-07-23 00:03:35 +0500

    5mb

--- routes/upload.js
+++ routes/upload.js
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB
+  limits: { fileSize: 20 * 1024 * 1024 }, // 20 MB
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith('image/')) cb(null, true)
     else cb(new Error('Only image files allowed'))
